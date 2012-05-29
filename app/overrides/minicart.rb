f2d2ace54bfe72c91404e25fe37ec41cf467baa1
Thomas Boltze <thomas.boltze@gmail.com>
2012-05-29 10:19:57 +0800

    Added raised $ for pricing Added minicart Added confirmation for add to mini cart Added :hover styles for buttons

---
+++ app/overrides/minicart.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => 'spree/shared/_nav_bar',
+                    :name => 'add_mini_cart',
+                    :insert_bottom => "#link-to-cart",
+                    :partial => "spree/shared/minicart")
