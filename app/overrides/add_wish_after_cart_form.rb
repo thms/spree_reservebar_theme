3a1c37d61885748bad05b92860efc0f4617f2d61
Thomas Boltze <thomas.boltze@gmail.com>
2012-06-06 14:04:08 +0800

    More tweaking for the wishlist overrides

--- app/overrides/add_wish_after_cart_form.rb
+++
@@ -1,6 +0,0 @@
-# Move the wishlist form before the cart form to make valid html
-Deface::Override.new(:virtual_path => "spree/products/show",
-                     :name => "add_wish_to_cart_form",
-                     :insert_before => "[data-hook='cart_form']",
-                     :partial => "spree/products/wishlist_form",
-                     :disabled => false)
