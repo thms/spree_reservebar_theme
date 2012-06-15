0c34936c528ce7ea9652a9be9d6a3d43a3acbb72
Thomas Boltze <thomas.boltze@gmail.com>
2012-06-15 17:39:23 +0800

    Replaced fonts Styling Placeholder for shipping instructions Added filters for search pages and top pages

--- app/overrides/remove_wish_from_cart_form.rb
+++ app/overrides/remove_wish_from_cart_form.rb
@@ -1,5 +1,5 @@
 # Remove the normal wishlist in the cart form, since we'll combine it with git options and whiskey of the month, easier to layout that way
 Deface::Override.new(:virtual_path => "spree/products/show",
                      :name => "add_wish_to_cart_form",
-                     :seqeunce => 100,
+                     :sequence => 100,
                      :disabled => true)
