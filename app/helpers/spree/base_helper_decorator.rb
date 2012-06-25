01d621907cb7f597584efe45beaf222a2e48ed71
Thomas Boltze <thomas.boltze@gmail.com>
2012-06-25 16:28:18 +0800

    Removed stuff form menu, added Gift Finder as non taxon. Added pinterest

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -12,6 +12,22 @@ Spree::BaseHelper.class_eval do
     end
   end
   
+  def menu_taxons_tree(root_taxon, current_taxon, max_level = 1, extra_items)
+    return '' if max_level < 1 || root_taxon.children.empty?
+    content_tag :ul, :class => 'taxons-list' do
+      menu = root_taxon.children.map do |taxon|
+        css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'current' : nil
+        content_tag :li, :class => css_class do
+         link_to(taxon.name, seo_url(taxon)) +
+         taxons_tree(taxon, current_taxon, max_level - 1)
+        end
+      end
+      menu << content_tag(:li, link_to(extra_items.first[0], extra_items.first[1]))
+      menu.join("\n").html_safe
+    end
+  end
+  
+  
   def short_breadcrumbs(taxon, separator="&nbsp;&raquo;&nbsp;")
     return "" if current_page?("/") || taxon.nil?
     separator = raw(separator)
