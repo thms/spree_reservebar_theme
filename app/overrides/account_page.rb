3913e15c9ad44add79854ab16e8c8e42ee5f0fbc
Jindong Yang <yangjindong@gmail.com>
2012-10-26 21:45:06 +0800

    updates for my account changes

--- app/overrides/account_page.rb
+++ app/overrides/account_page.rb
@@ -1,5 +1,26 @@
 Deface::Override.new(:virtual_path => "spree/users/show",
                      :name => "account_page_spacing",
                      :insert_before => "[data-hook='account_summary']",
-                     :text => "<style>\nh3 {margin-top:24px;}\n</style>\n",
+                     :text => "<style>\nh3 {margin-top:24px;} #content {width:980px;} table {width:100%;} #user-info td, #user_addresses td{width:50%;}\n</style>\n",
                      :disabled => false)
+                     
+Deface::Override.new(:virtual_path => "spree/users/show",
+                     :name => "account_page_title",
+                     :remove => "h3:contains('accurate_title')",
+                     :disabled => false)
+
+Deface::Override.new(:virtual_path => "spree/users/show",
+                     :name => "account_page_summary",
+                     :replace_contents => "[data-hook='account_summary']",
+                     :partial => "spree/shared/account_summary",
+                     :disabled => false)
+                     
+Deface::Override.new(:virtual_path => "spree/users/show",
+                     :name => "account_page_orders_title",
+                     :replace => "code[erb-loud]:contains('my_orders')",
+                     :text => "<%= t(:order_history) %>",
+                     :disabled => false)
+                     
+Deface::Override.new(:virtual_path => "spree/users/show",
+										 :name => "add_wishlists_to_account_my_orders",
+										 :disabled => true)
