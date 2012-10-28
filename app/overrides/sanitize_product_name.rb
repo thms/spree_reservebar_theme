22d64e680a1af0358cd867ac55ff08a768b3aba7
Jindong Yang <yangjindong@gmail.com>
2012-10-28 18:55:43 +0800

    update for the sanitize product name

--- app/overrides/sanitize_product_name.rb
+++ app/overrides/sanitize_product_name.rb
@@ -1,9 +1,3 @@
-Deface::Override.new(:virtual_path => 'spree/shared/_minicart_line_items',
-                    :name => 'sanitize_product_name_of_mini_cart',
-                    :replace_contents => "code[erb-loud]:contains('product_path')",
-                    :text => "link_to sanitize(variant.product.name, :tags => %w(sup)), product_path(variant.product)",
-                    :disabled => false)
-                    
 Deface::Override.new(:virtual_path => 'spree/orders/_line_item',
                     :name => 'sanitize_product_name_of_shopping_cart',
                     :replace_contents => "code[erb-loud]:contains('product_path')",
