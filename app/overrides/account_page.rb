c41f228c5aca583518fcbd91742103315ee9f53d
Thomas Boltze <thomas.boltze@gmail.com>
2012-10-29 18:08:40 +0800

    Tiny fixes to account page and button font size

--- app/overrides/account_page.rb
+++ app/overrides/account_page.rb
@@ -1,5 +1,17 @@
 Deface::Override.new(:virtual_path => "spree/users/show",
                      :name => "account_page_spacing",
                      :insert_before => "[data-hook='account_summary']",
-                     :text => "<style>\nh3 {margin-top:24px;}\n</style>\n",
+                     :text => "<style>\nh3 {margin-top:24px;} #content {width:980px;} table {width:100%;} #user-info td, #user_addresses td{width:50%;}\n</style>\n",
                      :disabled => false)
+
+Deface::Override.new(:virtual_path => "spree/users/show",
+                    :name => "account_page_summary",
+                    :replace_contents => "[data-hook='account_summary']",
+                    :partial => "spree/shared/account_summary",
+                    :disabled => false)
+
+Deface::Override.new(:virtual_path => "spree/users/show",
+                    :name => "account_page_orders_title",
+                    :replace => "code[erb-loud]:contains('my_orders')",
+                    :text => "<%= t(:order_history) %>",
+                    :disabled => false)
