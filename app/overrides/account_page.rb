f6b81b8553901144502a16046cf7ccb539b58a9c
Thomas Boltze <thomas.boltze@gmail.com>
2013-09-30 06:10:49 +0200

    Fixed the colors of the menu in bootstrap.css Fixed tile layouts Fixed aside Fixed taxon page Fixed account page

--- app/overrides/account_page.rb
+++ app/overrides/account_page.rb
@@ -2,7 +2,7 @@ Deface::Override.new(:virtual_path => "spree/users/show",
                      :name => "account_page_spacing",
                      :insert_before => "[data-hook='account_summary']",
                      :text => "<style>\nh3 {margin-top:24px;} #content {width:980px;} table {width:100%;} #user-info td, #user_addresses td{width:50%;}\n</style>\n",
-                     :disabled => false)
+                     :disabled => true)
 
 Deface::Override.new(:virtual_path => "spree/users/show",
                     :name => "account_page_summary",
